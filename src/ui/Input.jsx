7cd6111451c4b7cbfa3c2339928ec7e1517886c7
Morteza <mortazapor3@gmail.com>
2023-11-14 07:30:41 -0600

    Initial Commit

---
+++ src/ui/Input.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Input = styled.input`
+  border-radius: var(--border-radius-sm);
+  border: 1px solid var(--color-grey-300);
+background-color: var(--color-grey-0);
+  padding: .8rem 1.2rem;
+`
+
+export default Input;
